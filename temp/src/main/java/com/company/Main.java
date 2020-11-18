ecf850c3ef5832b9d8a750ad3a95e1e4bca7dc5b
gimadutdin <rustem.gimadutdinov@gmail.com>
2020-11-18 22:22:56 +0300

    fix parser 2

--- temp/src/main/java/com/company/Main.java
+++ temp/src/main/java/com/company/Main.java
@@ -13,7 +13,7 @@ public class Main {
         String token = "d1adb2dad1adb2dad1adb2da59d1d9d642dd1add1adb2da8e2b9de42ba576790649c910";
         String versionAPI = "5.103"; //current version
         String domain = "<group id>";
-        String exampleURL = "https://vk.com/nikita.vorobev99";
+        String exampleURL = "nikita.vorobev99";
         StringBuilder sb = new StringBuilder(exampleURL);
         //String connectionUrl = "jdbc:mysql://localhost:3306/test?useSSL=false";
         VKPageParser parser = new VKPageParser(token, versionAPI);
