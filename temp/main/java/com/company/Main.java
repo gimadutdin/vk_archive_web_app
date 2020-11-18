44f82081e6a411b0e2038e359a136832673850f2
vyatskiy <nikita.vorobev70999@gmail.com>
2020-11-18 20:14:39 +0300

    added the vk_page_parser

--- temp/main/java/com/company/Main.java
+++ temp/main/java/com/company/Main.java
@@ -6,11 +6,11 @@ import org.jsoup.nodes.Document;
 public class Main {
 
     public static void main(String[] args) {
-//    Document doc = Jsoup.connect("http://eclipse.org").get();
-//    String title = doc.title();
-//    System.out.println("Title : " + title);
-//    https://scontent-hel2-1.cdninstagram.com/v/t51.2885-15/e35/118963814_133075678501218_2503543476140269967_n.jpg?_nc_ht=scontent-hel2-1.cdninstagram.com&_nc_cat=106&_nc_ohc=t04SsR0b5lkAX8ENzLr&oh=ffb044fefdc55ee0c4656f6ab850c837&oe=5F887008
-      String token = "d1adb2dad1adb2dad1adb2da59d1d9d642dd1add1adb2da8e2b9de42ba576790649c910";
+        String token = "d1adb2dad1adb2dad1adb2da59d1d9d642dd1add1adb2da8e2b9de42ba576790649c910";
+        String versionAPI = "<5.103>"; //current version
+        String domain = "<group id>";
+        String connectionUrl = "jdbc:mysql://localhost:3306/test?useSSL=false";
 
+        (new VKPageParser(token, versionAPI, domain, new DataBase("root", "root", connectionUrl))).getUsers();
     }
 }
