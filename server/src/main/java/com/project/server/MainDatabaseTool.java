076691e173eb3a211acb190d6f4e57beee6773c8
gimadutdin <rustem.gimadutdinov@gmail.com>
2020-12-10 01:05:25 +0300

    server: added isUserPresent, readUserInfo, writeUserInfo + fixes

--- server/src/main/java/com/project/server/MainDatabaseTool.java
+++ server/src/main/java/com/project/server/MainDatabaseTool.java
@@ -5,6 +5,10 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.PreparedStatement;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 
 public class MainDatabaseTool {
@@ -25,15 +29,60 @@ public class MainDatabaseTool {
         return conn;
     }
 
+    public boolean isUserPresent(String user_id) {
+        String sql = String.format("SELECT user_id FROM users WHERE user_id = '%s'", user_id);
+        try (Connection conn = this.connect(); Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(sql)) {
+            if (rs.next()) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    public ObjectNode readUserInfo(String user_id) {
+        String sql = String.format("SELECT * FROM users WHERE user_id = '%s'", user_id);
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode objectNode = mapper.createObjectNode();
+        try (Connection conn = this.connect(); Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(sql)) {
+            if (rs.next()) {
+                //objectNode.put("user_id", rs.getString("user_id"));
+                objectNode.put("fio", rs.getString("fio"));
+                objectNode.put("city", rs.getString("city"));
+                objectNode.put("birthday", rs.getString("birthday"));
+                objectNode.put("study_place", rs.getString("study_place"));
+                objectNode.put("friends_number", rs.getInt("friends_number"));
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return objectNode;
+    }
+
+    public void writeUserInfo(ObjectNode user_info) {
+        String sql = "INSERT INTO USERS (USER_ID, FIO, CITY, BIRTHDAY, STUDY_PLACE, FRIENDS_NUMBER) VALUES (?, ?, ?, ?, ?, ?)";
+        try (Connection conn = this.connect(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, user_info.get("user_id").asText());
+            pstmt.setString(2, user_info.get("fio").asText());
+            pstmt.setString(3, user_info.get("city").asText());
+            pstmt.setString(4, user_info.get("birthday").asText());
+            pstmt.setString(5, user_info.get("study_place").asText());
+            pstmt.setInt(6, user_info.get("friends_number").asInt());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     public String selectAll(){
         String sql = "SELECT user_id, fio, city, birthday, study_place, friends_number FROM users";
         String result = "";
-        try (Connection conn = this.connect();
-             Statement stmt  = conn.createStatement();
-             ResultSet rs    = stmt.executeQuery(sql)){
-
-            // loop through the result set
-
+        try (Connection conn = this.connect(); Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(sql)) {
             while (rs.next()) {
                 result += (rs.getString("user_id") +  "\t" +
                         rs.getString("fio") + "\t" +
@@ -42,7 +91,6 @@ public class MainDatabaseTool {
                         rs.getString("study_place") + "\t" +
                         rs.getInt("friends_number") + "<br>");
             }
-
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
--- server/src/main/java/com/project/server/controller1/MainController.java
+++ server/src/main/java/com/project/server/controller1/MainController.java
@@ -30,31 +30,56 @@ public class MainController {
     @RequestMapping(value = "user-info/{id}", method = RequestMethod.GET) // todo change to POST
     public ObjectNode getItem(@PathVariable("id") String paramUserId) throws IOException {
 
+        // check if user already in our database
+        MainDatabaseTool mdt = new MainDatabaseTool();
+        boolean userPresent = mdt.isUserPresent(paramUserId);
+        // if user already exists in db then read its info
+        if (userPresent) {
+            ObjectNode resNode = mdt.readUserInfo(paramUserId);
+            resNode.put("was_in_db", true);
+            return resNode;
+        }
+
         String token = "d1adb2dad1adb2dad1adb2da59d1d9d642dd1add1adb2da8e2b9de42ba576790649c910";
         String versionAPI = "5.103"; //current version
 
         VKPageParser parser = new VKPageParser(token, versionAPI);
         Document respDoc = parser.connectDataUser(new StringBuilder(paramUserId));
-        //System.out.println(doc.text());
+        System.out.println(respDoc.text());
         String respJson = respDoc.text();
 
         JSONObject respJsonObject = new JSONObject(respJson);
+        if (!respJsonObject.has("response")) {
+            ObjectMapper mapper = new ObjectMapper();
+            ObjectNode objectNode = mapper.createObjectNode();
+            objectNode.put("error", "user_not_found");
+            return objectNode;
+        }
         JSONArray respArr = respJsonObject.getJSONArray("response");
         JSONObject mainInfo = respArr.getJSONObject(0);
 
-        String firstName = mainInfo.getString("first_name");
-        String lastName = mainInfo.getString("last_name");
-        String cityTitle = mainInfo.getJSONObject("city").getString("title");
-        String countyTitle = mainInfo.getJSONObject("country").getString("title");
-        String birthDate = mainInfo.getString("bdate");
+        String firstName = mainInfo.has("first_name") ? mainInfo.getString("first_name") : null;
+        String lastName = mainInfo.has("last_name") ? mainInfo.getString("last_name") : null;
+        String cityTitle = mainInfo.has("city") ? mainInfo.getJSONObject("city").getString("title") : null;
+        //String countyTitle = mainInfo.getJSONObject("country").getString("title");
+        String birthDate = mainInfo.has("bdate") ? mainInfo.getString("bdate") : null;
+        String studyPlace = mainInfo.has("university_name") ? mainInfo.getString("university_name") : null;
+
+        int friendsNumber = 0; // TODO
 
         ObjectMapper mapper = new ObjectMapper();
         ObjectNode objectNode = mapper.createObjectNode();
-        objectNode.put("first_name", firstName);
-        objectNode.put("last_name", lastName);
+        objectNode.put("user_id", paramUserId);
+        objectNode.put("fio", firstName + " " + lastName);
+        //objectNode.put("last_name", lastName);
         objectNode.put("city", cityTitle);
-        objectNode.put("country", countyTitle);
-        objectNode.put("birth_date", birthDate);
+        //objectNode.put("country", countyTitle);
+        objectNode.put("birthday", birthDate);
+        objectNode.put("study_place", studyPlace);
+        objectNode.put("friends_number", friendsNumber);
+        objectNode.put("was_in_db", false);
+
+        mdt.writeUserInfo(objectNode);
 
         return objectNode;
     }
